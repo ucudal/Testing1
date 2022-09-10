1ace2c8adf4ddde2cfed5feb136fad1d3d5b4800
Ignacio Perez <ignacioperez@192.168.17.128>
2022-09-09 23:58:41 -0300

    Finl

---
+++ tests/Library.Tests/DateFormatterTests.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+namespace Library.Tests;
+
+[TestFixture]
+public class DataFormatterTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
